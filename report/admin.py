29bd3722f5caa02824fea9af05f704ca96ebb506
habibahassan <halimaadan92@gmail.com>
2020-04-02 20:58:39 +0300

    added admin .py

--- report/admin.py
+++ report/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CaseStatus,Complaint,Fir,CopStatus,CaseClose
 
-# Register your models here.
+admin.site.register(CaseStatus)
+admin.site.register(Complaint)
+admin.site.register(Fir)
+admin.site.register(CopStatus)
+admin.site.register(CaseClose)
